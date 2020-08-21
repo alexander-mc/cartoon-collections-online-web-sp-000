ce292427f32b0978eec33a42c54d81c25cb2040c
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-21 01:45:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(array)
   array.each_with_index {|name, i| puts "#{i + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|word| 
 end
 
 def long_planeteer_calls# code an argument here
