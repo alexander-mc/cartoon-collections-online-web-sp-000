0c640392aa49d07689fb0efe3af28eabc22bf051
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-21 01:50:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def summon_captain_planet(array)
   array.map {|word| "#{word.capitalize}!"}
 end
 
-def long_planeteer_calls{array}
+def long_planeteer_calls(array)
   array.any?{|word| word.length > 4}
 end
 
