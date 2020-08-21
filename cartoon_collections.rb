6c4baf0022702560d4a54149a849807b9333d64b
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-21 01:43:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,5 @@
 def roll_call_dwarves(array)
-  array.each_with_index do |name, index|
-    puts "#{index + 1}. #{name}"
-  end
+  array.each_with_index {|name, i| puts "#{i + 1}. #{name}"
 end
 
 array = ["a","b"]
